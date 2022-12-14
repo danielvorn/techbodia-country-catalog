dc916001ad46e9a305bc42cb5d21e3656b105544
danielvorn <danielvorn@gmail.com>
2022-12-14 13:31:43 +0700

    fetching data and building pagination system

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { useEffect, useState } from "react";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <QueryClientProvider client={queryClient}>
+      <Catalog />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+function Catalog() {
+  const [countries, setCountries] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [countriesPerPage] = useState(25);
+  const indexOfLastPost = currentPage * countriesPerPage;
+  const indexOfFirstPost = indexOfLastPost - countriesPerPage;
+  const paginatedCountries = countries.slice(indexOfFirstPost, indexOfLastPost);
+
+  const { isLoading, error, data } = useQuery(
+    "countriesList",
+    () => fetch("https://restcountries.com/v3.1/all").then((res) => res.json()),
+    {
+      cacheTime: 60000,
+    }
+  );
+
+  useEffect(() => {
+    if (data) {
+      setCountries(data);
+    }
+  }, [data]);
+
+  const previousPage = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (currentPage !== Math.ceil(countries.length / countriesPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div>
+      <Paginate
+        postsPerPage={countriesPerPage}
+        totalPosts={countries.length}
+        paginate={paginate}
+        previousPage={previousPage}
+        nextPage={nextPage}
+      />
+      {JSON.stringify(paginatedCountries)}
     </div>
   );
 }
+
+const Paginate = ({
+  postsPerPage,
+  totalPosts,
+  paginate,
+  previousPage,
+  nextPage,
+}) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div className="pagination-container">
+      <ul className="pagination flex justify-end gap-3">
+        <li onClick={previousPage} className="page-number">
+          Prev
+        </li>
+        {pageNumbers.map((number) => (
+          <li
+            key={number}
+            onClick={() => paginate(number)}
+            className="page-number"
+          >
+            {number}
+          </li>
+        ))}
+        <li onClick={nextPage} className="page-number">
+          Next
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default App;
